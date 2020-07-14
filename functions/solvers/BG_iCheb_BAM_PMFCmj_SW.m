8a52990ac94d6a29092cbde5b2c243704169e7ef
buvoli <buvoli@uw.edu>
2020-07-13 21:43:13 -0700

    Bug Fix: Set L_y to SMVO for Adams method with SW endpoints, removed use_spmd parameter, and added node_ordering

--- functions/solvers/BG_iCheb_BAM_PMFCmj_SW.m
+++ functions/solvers/BG_iCheb_BAM_PMFCmj_SW.m
@@ -1,4 +1,4 @@
-function [BLKGenerator, MG] = BG_iCheb_BAM_PMFCmj_SW(node_precision, use_spmd)
+function [BLKGenerator, MG] = BG_iCheb_BAM_PMFCmj_SW(node_precision, node_ordering)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,15 +6,24 @@ if(nargin < 1)
     node_precision = 'double';
 end
 if(nargin < 2)
-    use_spmd = true;
+    node_ordering = 'inwards';
 end
 
-node_ordering  = 'inwards'; % use inwards for sweeping EPG endpoints
+if(~any(strcmp(node_ordering, {'inwards', 'outwards'})))
+    node_ordering  = 'inwards'; % use inwards for sweeping EPG endpoints
+    warning('Invalid node ordering; must be "inwards" or "outwards". Code will use inwards')
+end
+
+if(strcmp(node_ordering, 'inwards'))
+    name = 'iCheb-BAM-PMFCmj-SW-';
+elseif(strcmp(node_ordering, 'outwards'))
+    name = 'iCheb-OS-BAM-PMFCmj-SW-';
+end
 
 % -- select method --------------------------------------------------------------------------------
 MG = PBMGenerator(struct( ...
     'NodeSet_generator', IChebNSG(node_ordering, node_precision), ...
-    'ODEPoly_generator', Adams_PG('PMFO', 'PMFOmj', SweepingEPG(), 'diagonally_implicit') ...
+    'ODEPoly_generator', Adams_PG('SMVO', 'PMFOmj', SweepingEPG(), 'diagonally_implicit') ...
     ));
 
 % -- select extrapolation -------------------------------------------------------------------------
@@ -23,20 +32,14 @@ EG = PBMGenerator(struct( ...
     'ODEPoly_generator', YExtrap_PG('PMFO') ...
     ));
 
-if(use_spmd)
-    solver_class = @MutableDIPBLK;
-else
-    solver_class = @MutableDIBLK;
-end
-
-
+solver_class = @MutableDIBLK;
 BLKGenerator = BLKSolverGenerator( ...
     struct(...
     'solver_class',              solver_class, ...
     'method_generator',          MG, ...
     'extrapolator_generator',    EG, ...
     'output_coefficient_handle', @Output_IChebAdams_PMFC, ...
-    'name',                      'iCheb-BAM-PMFCmj-SW-' ...
+    'name',                      name ...
     ) ...
 );
 
